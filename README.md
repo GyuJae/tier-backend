cdd96f73315c519c78ede66f5ec058a65d2044da
Gyujae <wjdrbwo1206@naver.com>
2022-07-21 12:52:17 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Nestjs Boilerplate with graphql + prisma + plantscale
---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { PrismaModule } from './prisma/prisma.module';
+import { CoreModule } from './core/core.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      debug: process.env.NODE_ENV === 'development',
+      autoSchemaFile: true,
+      playground: false,
+      sortSchema: true,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+    PrismaModule,
+    CoreModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
